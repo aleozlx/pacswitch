5de88945da34d3a435e28e47f9d67a2ec8763149
aleozlx <aleozlx@yahoo.com>
2014-09-15 20:23:30 +0800

    pacscli4java becomes separate project

--- src/client api/PacswitchException.java
+++
@@ -1,9 +0,0 @@
-// Fork me on GitHub! https://github.com/aleozlx/pacswitch
-package com.aleozlx.pacswitch;
-
-public class PacswitchException extends Exception {
-	private static final long serialVersionUID = 6097161643360603291L;
-	public PacswitchException(){ super(); }
-	public PacswitchException(String message){ super(message); }
-	public PacswitchException(String message, Throwable cause){ super(message,cause); }
-}
