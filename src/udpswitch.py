0e31e35bd090447994e553ac6396401d5a94d993
aleozlx <aleozlx@yahoo.com>
2014-11-12 18:03:48 +0800

    module

--- src/udpswitch.py
+++ src/udpswitch.py
@@ -9,7 +9,7 @@ udpstreams=dict()
 def udpGC():
 	t_now=timemark()
 	# log.msg('udpGC tick '+str(t_now)+' '+str([(key,udpstreams[key][1]) for key in udpstreams]))
-	for k in [key for key in udpstreams.keys() if t_now-udpstreams[key][1] > 45.0]:
+	for k in [key for key in udpstreams if t_now-udpstreams[key][1] > 45.0]:
 		# log.msg('udpGC '+str(k))
 		del udpstreams[k]
 	reactor.callLater(60,udpGC) 
@@ -29,18 +29,20 @@ class UDPSwitch(protocol.DatagramProtocol):
 				udpstreams[index]=(ep,timemark())
 				self.assignIndex(ep, index)
 				while len(self.__agingtable)>20:
-					items=[(self.__agingtable[key][1],key) for key in self.__agingtable.keys()]
+					items=[(self.__agingtable[key][1],key) for key in self.__agingtable]
 					heapq.heapify(items)
 					key_m=items[0][1]
 					try: del self.__agingtable[key_m]
 					except: pass
 		else:
-			to=struct.unpack('>l',data[16:20])[0]
-			if to in udpstreams:
+			try:
+				to=struct.unpack('>l',data[16:20])[0]
 				ep2=udpstreams[to][0]
 				udpstreams[to]=(ep2,timemark())
-				try: self.transport.write(data,ep2)
-				except: log.msg('Transmission error')
+				self.transport.write(data,ep2)
+			except Exception, e:
+				pass
+				# log.msg('Transmission error')
 
 	def assignIndex(self, endpoint, index):
 		try: self.transport.write(struct.pack('>l', index), endpoint)
